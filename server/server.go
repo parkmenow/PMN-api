5e7b5792fb7463807268c3cbdd223c94d2be4ce7
Bharath Swamy <43372652+BharathTalakad@users.noreply.github.com>
2019-02-15 21:01:04 +0900

    Update server.go for graceful shutdown

--- server/server.go
+++ server/server.go
@@ -20,7 +20,23 @@ func StartServer(router *gin.Engine) {
 		Addr:    ":8080",
 		Handler: router,
 	}
-	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		log.Fatalf("listen: %s\n", err)
+	// Starts a go routine to start the server
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
 	}
+	log.Println("Server exiting")
 }
