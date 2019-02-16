307a6c10285166fae70924f2b1097c84b8143c5e
prashant maurya and Manas Agarwal <mauryaprashant.1992+manasagarwal96@gmail.com>
2019-02-17 00:16:26 +0900

    Adds seed-data for owners and edits users.json

--- db/db.go
+++ db/db.go
@@ -1,8 +1,8 @@
 package db
 
 import (
-	"github.com/github.com/parkmenow/PMN-api/models"
 	"github.com/jinzhu/gorm"
+	"github.com/parkmenow/PMN-api/models"
 )
 
 // DB is the database connection
@@ -11,5 +11,5 @@ var DB *gorm.DB
 // Init sets the given database connection as the de-facto connection for this app
 func Init(db *gorm.DB) {
 	DB = db
-	DB.AutoMigrate(&models.User{})
+	DB.AutoMigrate(&models.User{}, &models.Owner{})
 }
---
+++ db/seed/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+
+	"github.com/parkmenow/PMN-api/db"
+	"github.com/parkmenow/PMN-api/models"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+func main() {
+	var users []models.User
+	var owners []models.Owner
+	getData("data/users.json", &users)
+	getData("data/owners.json", &owners)
+
+	database, err := gorm.Open("sqlite3", "pmn.db")
+	if err != nil {
+		panic("failed to establish database connection")
+	}
+	defer database.Close()
+	db.Init(database)
+
+	for _, user := range users {
+		db.DB.Create(&user)
+	}
+	for _, owner := range owners {
+		db.DB.Create(&owner)
+	}
+
+	println("Done, copy pmn.db to root folder")
+}
+
+func getData(fileName string, v interface{}) {
+	file, _ := os.Open(fileName)
+	defer file.Close()
+	byteValue, _ := ioutil.ReadAll(file)
+	json.Unmarshal(byteValue, v)
+}
