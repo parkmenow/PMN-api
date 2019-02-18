c92c9bf2eb47820291f630dd81e5b5ee1c9d2a25
Manas and prashant maurya <manasagarwal96+mauryaprashant.1992@gmail.com>
2019-02-18 15:58:09 +0900

    Adds POST /dashboard/regSlot API endpoint

--- server/handlers.go
+++ server/handlers.go
@@ -121,10 +121,18 @@ func regParkingSpot(c *gin.Context) {
 
 func regSpot(c *gin.Context) {
 	var spot models.Spot
-	//var spots []models.Spot
+
 	db := getDB(c)
 	c.BindJSON(&spot)
 	db.Create(&spot)
-	//db.Where("property_id = ?", spot.PropertyID).Find(&spots)
 	c.JSON(200, "Successfully Added Spot")
+}
+
+func regSlot(c *gin.Context) {
+	var slot models.Slot
+
+	db := getDB(c)
+	c.BindJSON(&slot)
+	db.Create(&slot)
+	c.JSON(200, "Successfully Added Slot")
 }
