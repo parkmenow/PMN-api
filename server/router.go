86064b4e1a8408d0e72b53d4f4231d2cef7a18a8
Manas and prashant maurya <manasagarwal96+mauryaprashant.1992@gmail.com>
2019-02-18 22:28:16 +0900

    Resolves conflict in routes.go

--- server/router.go
+++ server/router.go
@@ -17,17 +17,9 @@ func defineRoutes(router *gin.Engine) {
 	user.Use(authMiddleware.MiddlewareFunc())
 	user.GET("/", getUserFirstName)
 	user.Use(authMiddleware.MiddlewareFunc())
-<<<<<<< Updated upstream
-	user.POST("/parkmenow", fetchParkingSpots)
-	user.POST("/regparking", regParkingSpot)
-	user.POST("/regSpot", regSpot)
-	user.POST("/regSlot", regSlot)
-	user.PATCH("/modifySpot", modifySpot)
-=======
 	user.POST("/:id/parkmenow", fetchParkingSpots)
 	user.POST("/:id/regparking", regParkingSpot)
 	user.POST("/:id/regparking/regSpot/:spot_id", regSpot)
 	user.POST("/:id/regparking/regSpot/:spot_id/regSlot/:slot_id", regSlot)
 	user.PATCH("/:id/listings/modifySpot", modifySpot)
->>>>>>> Stashed changes
 }
