378da2cf4875dd2d167abdc878e0126fb9428f00
codechaitu <codechaitu@mercari.com>
2019-02-20 15:32:55 +0900

    Added comments for pmn-api.go and made a change in checking if case

--- pmn-api.go
+++ pmn-api.go
@@ -13,9 +13,13 @@ import (
 )
 
 func main() {
+	// 1) Get the environment variable if any set for which database and what the database url is.
 	DATABASE := os.Getenv("DB_DRIVER")
 	databaseURL := os.Getenv("DATABASE_URL")
-	if DATABASE == "" {
+
+	// 2) If any of the environment variables are not set, then check for a .env file, that may contain required variables
+	if DATABASE == "" || databaseURL == "" {
+		// Load the environment variables from .env and set the variables
 		err := godotenv.Load(".env")
 		if err != nil {
 			log.Fatal("Error loading .env file")
@@ -23,12 +27,15 @@ func main() {
 		DATABASE = os.Getenv("DB_DRIVER")
 		databaseURL = os.Getenv("DATABASE_URL")
 	}
+	// 3) Connect to the gorm database
 	database, err := gorm.Open(DATABASE, databaseURL)
 	if err != nil {
 		panic("failed to establish database connection")
 	}
 	defer database.Close()
 	db.Init(database)
+
+	// 4) Creating a server with database parameter, so that server initalizes the database.
 	router := server.CreateRouter(database)
 	server.StartServer(router)
 }
