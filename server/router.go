9b970b8af6a0633352c0542d394f1e50b870728f
singh-shubham <singhshubham.iitkgp@gmail.com>
2019-02-24 01:02:05 +0900

    change parking to property in api call

--- server/router.go
+++ server/router.go
@@ -25,7 +25,8 @@ func defineRoutes(router *gin.Engine) {
 		user.PATCH("/payment", payment)
 		user.PATCH("/paybywallet", paymentByWallet)
 		user.POST("/cancelBooking", cancelBooking)
-		user.DELETE("/listings/parkings/:parking_id/spot/:spot_id/slot/:slot_id", deleteSlot)
-		user.DELETE("/listings/parkings/:parking_id/spot/:spot_id", deleteSpot)
+		user.DELETE("/listings/property/:property_id/spot/:spot_id/slot/:slot_id", deleteSlot)
+		user.DELETE("/listings/property/:property_id/spot/:spot_id", deleteSpot)
+		user.DELETE("/listings/property/:property_id", deleteSpot)
 	}
 }
