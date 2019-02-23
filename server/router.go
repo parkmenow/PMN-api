ffc483df26384453c8020546bdb7cc00b2541de3
singh-shubham <singhshubham.iitkgp@gmail.com>
2019-02-24 00:37:36 +0900

    Add endpoint for deleteing slot

--- server/router.go
+++ server/router.go
@@ -25,5 +25,6 @@ func defineRoutes(router *gin.Engine) {
 		user.PATCH("/payment", payment)
 		user.PATCH("/paybywallet", paymentByWallet)
 		user.POST("/cancelBooking", cancelBooking)
+		user.DELETE("/listings/parkings/:parking_id/spot/:spot_id/slot/:slot_id", deleteSlot)
 	}
 }
